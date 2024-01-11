436f59d9102552ff2d84995bec3ea5bafa9250ef
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-01-11 20:54:12 +0000

    haciendo cambios

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-let numeros = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 let iconos = ["♦", "♥", "♠", "♣"]
-let cartas = 
+let cartas = []
+
+let draw = document.querySelector('#draw')
+let sort = document.querySelector('#sort')
+let contenedor = document.querySelector('.contendor')
+
+draw.addEventListener('click', generarCartas)
+sort.addEventListener('click', generarCartas)
+
+function generarCartas () {
+let card = document.createElement("div");
+card.className = "card";
+let iconoArriba = document.createElement("div");
+iconoArriba.className = "icono-arriba";
+let iconoAbajo = document.createElement("div");
+iconoAbajo.className = "icono-abajo";
+let cardnumero = document.createElement("div");
+cardnumero.className = "numero";
+
+
+
+iconoArriba.classList.add()
+iconoAbajo.classList.add()
+cardnumero.classList.add()
+
+contenedor.appenChild()
+}
+
+
+function generariconos() {
+    let numero = Math.floor(Math.random() * iconos.length);
+    return numero;
+  }
+
+  function generarnumeros() {
+    let numero = Math.floor(Math.random() * iconos.length);
+    return numero;
+  }
+
+  
