43c428e5937527b882dd47a5e44585cb25d2ae93
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-01-12 18:08:47 +0000

    agregando cambios al codigo

--- index.js
+++ index.js
@@ -9,51 +9,58 @@ let contenedor = document.querySelector('.contenedor')
 draw.addEventListener('click', generarCartas)
 sort.addEventListener('click', generarCartas)
 
+
+function generaricono() {
+  let numero = Math.floor(Math.random() * iconos.length);
+  return numero;
+}
+
+function generarnumero() {
+  let numero = Math.floor(Math.random() * numeros.length);
+  return numero;
+}
+
+
 function generarCartas() {
-  let card = document.createElement('div')
-  card.classList.add('card')
+  let numeroCartas = parseInt(document.getElementById('amountcards').value);
+  for (let i = 0; i < numeroCartas; i++) {
+    let icono = iconos [generaricono()]
+    let num = numeros [generarnumero()]
 
-  let iconoarriba = document.createElement('div')
-  iconoarriba.classList.add('icono-arriba')
-  iconoarriba.innerHTML = '♣'
+    let card = document.createElement('div')
+    card.classList.add('card')
 
-  let numero = document.createElement('div')
-  numero.classList.add('numero')
-  numero.innerHTML = 'A'
+    let iconoarriba = document.createElement('div')
+    iconoarriba.classList.add('icono-arriba')
+    iconoarriba.innerHTML = icono
 
-  let iconoabajo = document.createElement('div')
-  iconoabajo.classList.add('icono-abajo')
-  iconoabajo.innerHTML = '♣'
+    let numero = document.createElement('div')
+    numero.classList.add('numero')
+    numero.innerHTML = num
 
-
-  contenedor.appendChild(card)
-  card.appendChild(iconoarriba)
-  card.appendChild(numero)
-  card.appendChild(iconoabajo)
-
+    let iconoabajo = document.createElement('div')
+    iconoabajo.classList.add('icono-abajo')
+    iconoabajo.innerHTML = icono
 
 
-}
+    contenedor.appendChild(card)
+    card.appendChild(iconoarriba)
+    card.appendChild(numero)
+    card.appendChild(iconoabajo)
 
-let numeroCartas = parseInt(document.getElementbyId('amountcards').value);
-for (let i = 0; i < numeroCartas; i++) {
+    if (icono == "♦" || icono == "♥") {
+      iconoarriba.style.color = "red";
+      iconoabajo.style.color = "red";
+    } else {
+      iconoarriba.style.color = "black";
+      iconoabajo.style.color = "black";
+    }
 
-  function generaricono() {
-    let numero = Math.floor(Math.random() * iconos.length);
-    return numero;
   }
-
-  function generarnumero() {
-    let numero = Math.floor(Math.random() * numeros.length);
-    return numero;
-  }
-
 }
 
-if (icono == "♦" || icono == "♥") {
-  iconoarriba.style.color = "red";
-  iconoabajo.style.color = "red";
-} else {
-  iconoarriba.style.color = "black";
-  iconoabajo.style.color = "black";
-}
+
+
+
+
+
